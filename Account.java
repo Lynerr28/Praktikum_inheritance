89638ffd7050739cadd4b1516cc2c7e7f5477b75
Lynerr28 <114018054+Lynerr28@users.noreply.github.com>
2022-10-25 21:58:28 +0700

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum_inheritance;
+
+/**
+ *
+ * @author fiezb
+ */
+public class Account {
+    
+}
